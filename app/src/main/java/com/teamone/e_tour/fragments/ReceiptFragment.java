b7eb5439e52f3124f0be8d8763d63b9b1a8cd3fa
ngphth1312 <ngphth1312@gmail.com>
2023-04-25 01:10:13 +0700

    feature:adding Receipt fragment

--- app/src/main/java/com/teamone/e_tour/fragments/ReceiptFragment.java
+++ app/src/main/java/com/teamone/e_tour/fragments/ReceiptFragment.java
@@ -1,13 +1,24 @@
 package com.teamone.e_tour.fragments;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
-
+import com.teamone.e_tour.R;
+import com.teamone.e_tour.api.ticket.BookTicketApi;
 import com.teamone.e_tour.databinding.FragmentReceiptBinding;
+import com.teamone.e_tour.dialogs.LoadingDialog;
+import com.teamone.e_tour.entities.Ticket;
+import com.teamone.e_tour.models.BookingDataManager;
+import com.teamone.e_tour.utils.Formatter;
+
 
 public class ReceiptFragment extends Fragment {
 
@@ -24,6 +35,44 @@ public class ReceiptFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         FragmentReceiptBinding binding = FragmentReceiptBinding.inflate(inflater, container, false);
+        BookTicketFragment.viewThirdTab();
+
+        LoadingDialog dialog = new LoadingDialog(requireActivity());
+        dialog.showLoading("We are preparing for your ticket");
+
+        binding.backToHomeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(requireActivity(), R.id.home_wrapper).navigate(R.id.action_bookTicketFragment_to_homeFragment);
+            }
+        });
+
+        BookTicketApi.getInstance(getActivity()).getData().observe(getViewLifecycleOwner(), new Observer<Ticket>() {
+            @Override
+            public void onChanged(Ticket ticket) {
+                if (ticket == null) return;
+
+                BookingDataManager.getInstance().setBookedTicket(ticket);
+
+                dialog.dismiss();
+
+                binding.totalPrice.setText(Formatter.toCurrency(ticket.getPrice()));
+                binding.ticketId.setText(ticket.get_id());
+                binding.customerName.setText(ticket.getFullName());
+                binding.phoneNumber.setText(ticket.getPhoneNumber());
+                binding.bookingDate.setText(Formatter.dateToDateWithHourString(ticket.getCreatedAt()));
+            }
+        });
+
+        BookingDataManager.getInstance().getErrorMessage().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                if (s.length() == 0) return;
+                dialog.dismiss();
+                NavHostFragment.findNavController(ReceiptFragment.this).popBackStack();
+                NavHostFragment.findNavController(ReceiptFragment.this).popBackStack();
+            }
+        });
 
         return binding.getRoot();
     }
