ef744d2990aef0f000a41a9ca6d8964cb8ccc542
Hoang Hy <hoanghy01122003@gmail.com>
2023-04-25 02:23:51 +0700

    feat: add AccountRegistrationFragment

--- app/src/main/java/com/teamone/e_tour/fragments/AccountRegistrationFragment.java
+++ app/src/main/java/com/teamone/e_tour/fragments/AccountRegistrationFragment.java
@@ -2,14 +2,20 @@ package com.teamone.e_tour.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
+import com.teamone.e_tour.R;
+import com.teamone.e_tour.activities.RegistrationActivity;
 import com.teamone.e_tour.databinding.FragmentAccountRegistrationBinding;
+
+import java.util.Objects;
 
 
 public class AccountRegistrationFragment extends Fragment {
@@ -29,6 +35,21 @@ public class AccountRegistrationFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentAccountRegistrationBinding.inflate(inflater, container, false);
+        RegistrationActivity activity = (RegistrationActivity) getActivity();
+        binding.setViewModel(Objects.requireNonNull(activity).getRegistrationViewModel());
+        binding.accountRegistrationNextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment.findNavController(AccountRegistrationFragment.this).navigate(R.id.action_accountRegistrationFragment_to_basicInforRegistrationFragment);
+            }
+        });
+
+        binding.backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
 
         return binding.getRoot();
     }
