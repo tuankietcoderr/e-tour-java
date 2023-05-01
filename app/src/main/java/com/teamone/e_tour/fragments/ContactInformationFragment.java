dc53b30ff61db1d8da331f99bb39c2a88d27fc46
Tuyt Mai <tranthituyetmai7430@gmail.com>
2023-05-02 02:32:26 +0700

    feat: add new fragment

--- app/src/main/java/com/teamone/e_tour/fragments/ContactInformationFragment.java
+++ app/src/main/java/com/teamone/e_tour/fragments/ContactInformationFragment.java
@@ -1,14 +1,22 @@
 package com.teamone.e_tour.fragments;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
+import com.teamone.e_tour.R;
 import com.teamone.e_tour.databinding.FragmentContactInformationBinding;
+import com.teamone.e_tour.entities.UserProfile;
+import com.teamone.e_tour.models.BookingDataManager;
+import com.teamone.e_tour.models.UserProfileManager;
+
+import java.util.Objects;
 
 public class ContactInformationFragment extends Fragment {
 
@@ -25,6 +33,32 @@ public class ContactInformationFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         FragmentContactInformationBinding binding = FragmentContactInformationBinding.inflate(inflater, container, false);
+
+        BookingDataManager bookingDataManager = BookingDataManager.getInstance();
+        binding.fullName.setText(bookingDataManager.getTicketData().ticketInfo.getFullName());
+        binding.phoneNumber.setText(bookingDataManager.getTicketData().ticketInfo.getPhoneNumber());
+        binding.email.setText(bookingDataManager.getTicketData().ticketInfo.getEmail());
+
+        binding.useYourInfo.setOnClickListener(v -> {
+            UserProfile userProfile = UserProfileManager.getInstance(requireActivity()).getUserProfile();
+
+            bookingDataManager.getTicketData().ticketInfo.setFullName(Objects.requireNonNull(userProfile.getFullName()));
+            bookingDataManager.getTicketData().ticketInfo.setPhoneNumber(Objects.requireNonNull(userProfile.getPhoneNumber()));
+            bookingDataManager.getTicketData().ticketInfo.setEmail(Objects.requireNonNull(userProfile.getEmail()));
+
+            binding.fullName.setText(BookingDataManager.getInstance().getTicketData().ticketInfo.getFullName());
+            binding.phoneNumber.setText(BookingDataManager.getInstance().getTicketData().ticketInfo.getPhoneNumber());
+            binding.email.setText(BookingDataManager.getInstance().getTicketData().ticketInfo.getEmail());
+        });
+
+        binding.nextBtn.setOnClickListener(v -> {
+            bookingDataManager.getTicketData().ticketInfo.setFullName(Objects.requireNonNull(binding.fullName.getText()).toString());
+            bookingDataManager.getTicketData().ticketInfo.setPhoneNumber(Objects.requireNonNull(binding.phoneNumber.getText()).toString());
+            bookingDataManager.getTicketData().ticketInfo.setEmail(Objects.requireNonNull(binding.email.getText()).toString());
+            NavHostFragment.findNavController(ContactInformationFragment.this).popBackStack();
+        });
+
+        binding.topAppBar.setOnClickListener(v -> NavHostFragment.findNavController(ContactInformationFragment.this).popBackStack());
 
         return binding.getRoot();
     }
