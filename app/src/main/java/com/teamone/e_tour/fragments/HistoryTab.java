dbd0ef40986726688c74f8c534083b2dde072d52
Le Doan Tan Tri <ledoantantri@gmail.com>
2023-05-10 03:20:57 +0700

    feat: add HistoryTab fragment

--- app/src/main/java/com/teamone/e_tour/fragments/HistoryTab.java
+++ app/src/main/java/com/teamone/e_tour/fragments/HistoryTab.java
@@ -1,13 +1,28 @@
 package com.teamone.e_tour.fragments;
 
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
-
+import com.teamone.e_tour.R;
+import com.teamone.e_tour.adapters.BookedTicketAdapter;
+import com.teamone.e_tour.api.ticket.ViewBookedTicketApi;
 import com.teamone.e_tour.databinding.FragmentHistoryTabBinding;
+import com.teamone.e_tour.dialogs.LoadingDialog;
+import com.teamone.e_tour.models.BookedTicketManager;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class HistoryTab extends Fragment {
 
@@ -25,6 +40,46 @@ public class HistoryTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         FragmentHistoryTabBinding binding = FragmentHistoryTabBinding.inflate(inflater, container, false);
+
+        requireActivity().findViewById(R.id.bottom_navigation).setVisibility(View.VISIBLE);
+        requireActivity().findViewById(R.id.home_wrapper).setPadding(0, 0, 0, 180);
+
+        BookedTicketAdapter incomingTourAdapter = new BookedTicketAdapter(this);
+        BookedTicketAdapter visitedTourAdapter = new BookedTicketAdapter(this);
+        LoadingDialog dialog = new LoadingDialog(requireActivity());
+        dialog.showLoading("Loading data");
+        BookedTicketManager.getInstance((AppCompatActivity) getActivity()).getBookedTickets().observe(getViewLifecycleOwner(), new Observer<ArrayList<ViewBookedTicketApi.ResponseData.Ticket>>() {
+            @Override
+            public void onChanged(ArrayList<ViewBookedTicketApi.ResponseData.Ticket> tickets) {
+                if (tickets == null) return;
+                binding.swiperefresh.setRefreshing(false);
+                dialog.dismiss();
+                ArrayList<ViewBookedTicketApi.ResponseData.Ticket> incomingTourList = new ArrayList<>();
+                ArrayList<ViewBookedTicketApi.ResponseData.Ticket> visitedTourList = new ArrayList<>();
+                tickets.forEach(ticket -> {
+                    if (ticket.tourId.from.before(new Date()))
+                        visitedTourList.add(ticket);
+                    else
+                        incomingTourList.add(ticket);
+                });
+                incomingTourAdapter.setTickets(incomingTourList);
+                visitedTourAdapter.setTickets(visitedTourList);
+            }
+        });
+
+        binding.incomingTourList.setAdapter(incomingTourAdapter);
+        binding.incomingTourList.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        binding.incomingTourList.setNestedScrollingEnabled(false);
+        binding.visitedTourList.setAdapter(visitedTourAdapter);
+        binding.visitedTourList.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        binding.visitedTourList.setNestedScrollingEnabled(false);
+
+        binding.swiperefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                BookedTicketManager.getInstance((AppCompatActivity) getActivity()).viewBookedTickets();
+            }
+        });
 
         return binding.getRoot();
     }
